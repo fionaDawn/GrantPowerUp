8cb1c352e17707656ab5600812b62ee5b9ab7b02
Fiona Dawn Potestas <fdpotestas@administrators-Mac-mini-2.local>
2016-09-29 08:14:59 +0800

    test

--- js/grant-type.js
+++ js/grant-type.js
@@ -4,14 +4,20 @@ var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
 var gTypeSelector = document.getElementById('grant-type');
+var purposeInput = document.getElementById('purpose');
 
 t.render(function(){
   return Promise.all([
-    t.get('card', 'private', 'gType')
+    t.get('card', 'private', 'gType'),
+    t.get('card', 'private', 'purpose')
   ])
-  .spread(function(savedGtype){
+  .spread(function(savedGtype, savedPurpose){
     if(savedGtype && /[a-z]+/.test(savedGtype)){
       gTypeSelector.value = savedGtype;
+    }
+
+    if(savedPurpose && /[a-z]+/.test(savedPurpose)){
+      purposeInput.value = savedPurpose;
     }
   })
   .then(function(){
@@ -22,6 +28,9 @@ t.render(function(){
 
 document.getElementById('save').addEventListener('click', function(){
   return t.set('card', 'private', 'gType', gTypeSelector.value)
+  .then(function(){
+    return t.set('card', 'private', 'purpose', purposeInput.value) 
+  })
   .then(function(){
     t.closePopup();
   })
