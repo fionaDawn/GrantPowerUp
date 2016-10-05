01d4dbf340868c3d8eda897447566d244bbdaa68
Fiona Dawn Potestas <fdpotestas@administrators-Mac-mini-2.local>
2016-10-06 05:16:48 +0800

    added grant-flow

--- js/template.js
+++ js/template.js
@@ -142,7 +142,7 @@ var formatNPSUrl = function(t, url){
 
 var boardButtonCallback = function(t){
   return t.boardBar({
-    url: 'https://grant-flow.firebaseapp.com/#/new',
+    url: 'https://grant-flow.firebaseapp.com/#/grant/new',
     height: 500
   });
 };
